875c12c925c6586cb50c708681cfb344cde293d4
Alex Elliott <alexelliott22@gmail.com>
2022-02-18 14:33:21 -0500

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,7 +15,7 @@ function About() {
                 </Col>
                 <Col lg={8} className='d-flex align-items-center justify-content-center text-center'>
                     <p >
-                        Front-end web developer leveraging an accouting background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the Vanderbilt University, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
+                        Front-end web developer leveraging an accounting background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the Vanderbilt University, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
                     </p>
                 </Col>
 
